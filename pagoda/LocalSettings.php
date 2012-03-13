61eddccca5245ed06bf2ee29db515df6ae92830b
Mark Parson <mark@delorum.com>
2012-03-13 16:35:51 -0600

    added logo

--- pagoda/LocalSettings.php
+++ pagoda/LocalSettings.php
@@ -36,7 +36,8 @@ $wgStylePath        = "$wgScriptPath/skins";
 
 ## The relative URL path to the logo.  Make sure you change this from the default,
 ## or else you'll overwrite your logo when you upgrade!
-$wgLogo             = "$wgStylePath/common/images/wiki.png";
+## $wgLogo             = "$wgStylePath/common/images/wiki.png";
+$wgLogo             = "pagoda/logo.png";
 
 ## UPO means: this is also a user preference option
 
